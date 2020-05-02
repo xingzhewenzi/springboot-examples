ca62d709e6b927b534d1d4f08c2da02f373e93aa
xingzhe <you@example.com>
2020-05-02 23:24:27 +0800

    mybatisPlus 代码生成器

---
+++ springboot-mybatis-plus-generator/src/test/java/cloud/xingzhe/springbootmybatisplusgenerator/SpringbootMybatisPlusGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package cloud.xingzhe.springbootmybatisplusgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisPlusGeneratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
